c2362cf5a22e8a7efc1d919a639e2534f630ab5e
Zack Tylor <taylor.zackory@gmail.com>
2017-08-10 08:40:37 -0500

    Add methods to generate board after it has been added to the databse.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,27 @@
 class Board < ApplicationRecord
+  attr_reader :grid
+  after_create :generate_board
+
+  private
+  def generate_board
+    # Generates a board hash after the board object has been added to the database.
+    x_axis_letters = %w(a b c d e f g h i j)
+    y_axis_nums = %w(0 1 2 3 4 5 6 7 8 9)
+
+    @grid = {}
+
+    x_axis_letters.each do |x|
+      y_axis_nums.each do |y|
+        add_to_grid(x + y) # x + y will eval to: a1, b7 etc...
+      end
+    end
+  end
+
+  def add_to_grid(coordinates)
+    # Takes in coordinates and adds them to the boards grid.
+    self.grid[coordinates] = { hit?: false,
+                               boat?: false,
+                               }
+  end
+
 end
