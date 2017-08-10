85d8a69fe5093cd428c592cbf2e24ab5e9a31a3c
Alan English <email": null>
2017-08-10 17:48:13 -0500

    add most recent shot method, adjust specs for board, add asscociation for players and shots

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,3 +1,8 @@
 class Shot < ApplicationRecord
   belongs_to  :board
+  has_one :player, through: :board
+
+  def self.most_recent
+    self.last
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many  :boards, foreign_key: :player_id
   has_many  :created_games, class_name: "Game", foreign_key: :creator_id
   has_many  :games, through: :boards, source: :game
+  has_many :shots, through: :boards
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
