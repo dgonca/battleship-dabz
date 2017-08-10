92c6f5eaed836c7122fdf580180cb263a6ddbc98
Dominique Goncalves & Zackory Taylor <pair+dgonca.zacktaylor@devbootcamp.com>
2017-08-10 11:14:17 -0500

    CREATE BOARD using grid and looping to create grid

---
+++ app/controllers/boards/boards_controller.rb
@@ -0,0 +1,13 @@
+class BoardsController < ApplicationController
+
+  def index
+    p "***************************************************"
+
+    # @board = Board.create
+    # logger.info @board
+  end
+
+  def show
+    @board = Board.create
+  end
+end
