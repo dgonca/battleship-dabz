f5ca43072ed95b272b940fef6deca637331b9f61
Alan English <email": null>
2017-08-10 11:22:01 -0500

    added functionality to successfully register, login and logout user, fixed links on homepage however create game and current game commented out until resource made, create users controller, show view and custom user show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
 
   # devise_for :users
+  # get '/register', to: "devise/registrations#new"
+
 
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+
+  get '/users/:id', to: "users#show", as: 'user'
 
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
