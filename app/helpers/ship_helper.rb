360c971150cbb9cb29e005f600942237814bc4b8
dgonca <domstar1595@yahoo.com>
2017-08-11 15:50:06 -0500

    nested forms with array working, needed fields_for: ships[], ship to distinguish that multiple ships are going into this fields form

--- app/helpers/ship_helper.rb
+++ app/helpers/ship_helper.rb
@@ -2,17 +2,14 @@ module ShipHelper
 
 	def generate_ships(board)
 		ships = []
-		p 'boop'
 		ShipType.all.each do |type|
 			ships << Ship.create(ship_type: type, board: board)
 		end
-		# p ships
-		p '````````````````````````````````'
-		p ships 
+		ships
 	end
 
-	def find_orientation(start_point)
+	# def find_orientation(start_point)
 
-	end
+	# end
 
 end
